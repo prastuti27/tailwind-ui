249d37060aec953c6e6b86cff4f44e8093478030
Prastuti <praskim143@gmail.com>
2024-06-21 13:40:40 +0545

    first commit tailwind design

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+
+    theme: {
+      colors: {
+      
+        purple:  "#37447E",
+        darkpurple: "#111b47",
+        lightpurple: "#505F98",
+        black:"#000000",
+        white:"#ffffff",
+        footer:"#E7ECFF",
+        grey:"#CDD1D4"
+
+      },
+      extend: {},
+    },
+
+    
+
+  plugins: [],
+}
